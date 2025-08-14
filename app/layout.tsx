3a6e7599eb4715909b0560d82bc15f04ba689818
Emoulya <imtiyazsabda0628@gmail.com>
2025-08-15 00:03:06 +0700

    style: module font geist

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type React from "react";
 import type { Metadata } from "next";
 import "./globals.css";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import { AuthProvider } from "@/hooks/use-auth";
 import { DataProvider } from "@/contexts/data-context";
 import { Toaster } from "@/components/ui/sonner";
@@ -8,7 +10,6 @@ import { Toaster } from "@/components/ui/sonner";
 export const metadata: Metadata = {
 	title: "PTC Monitoring System",
 	description: "Gas Storage Monitoring System",
-	generator: "v0.dev",
 };
 
 export default function RootLayout({
@@ -18,7 +19,15 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<head></head>
+			<head>
+				<style>{`
+html {
+  font-family: ${GeistSans.style.fontFamily};
+  --font-sans: ${GeistSans.variable};
+  --font-mono: ${GeistMono.variable};
+}
+        `}</style>
+			</head>
 			<body>
 				<AuthProvider>
 					<DataProvider>
